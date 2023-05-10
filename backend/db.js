57e2e2ad61a663ba2dfed19cc0f5f6b859e50e05
Dhruv Gogna <dhruvgogna01@gmail.com>
2023-05-11 00:30:25 +0530

    changed database to atlas

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@
 const mongoose=require("mongoose");
 
 const connectToMongo = ()=>{
-    mongoose.connect("mongodb://localhost:27017/cabSystemDB")
+    // mongoose.connect("mongodb://localhost:27017/cabSystemDB")
+    mongoose.connect("mongodb+srv://admin-dhruv:Test123@cluster0.zmald.mongodb.net/cabSystemDB")
 }
 
 module.exports = connectToMongo;
